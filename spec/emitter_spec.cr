80434b4c9f62df565a8975aeee34b402e8581b55
lowkey <lowkey@kaffeinlabs.com>
2026-05-29 21:20:54 -0700

    test(spec-emitter): test sequence kyaml emission

--- spec/emitter_spec.cr
+++ spec/emitter_spec.cr
@@ -45,4 +45,46 @@ describe "KYAML::Emitter" do
       KYAML::Any.new("x").to_yaml.should eq(%("x"))
     end
   end
+
+  describe "sequence rendering" do
+    it "renders an empty sequence inline" do
+      KYAML.emit([] of KYAML::Any).should eq("[]")
+    end
+
+    it "renders a single-element sequence as multi-line with trailing comma" do
+      kyaml = <<-KYAML
+      [
+        "foo",
+      ]
+      KYAML
+      seq = [KYAML::Any.new("foo")]
+      KYAML.emit(seq).should eq(kyaml)
+    end
+
+    it "renders mixed scalar elements as one per line" do
+      kyaml = <<-KYAML
+    [
+      "foo",
+      42,
+      true,
+    ]
+    KYAML
+      seq = [KYAML::Any.new("foo"), KYAML::Any.new(42), KYAML::Any.new(true)]
+      KYAML.emit(seq).should eq(kyaml)
+    end
+
+    it "indents nested sequences with two spaces per level" do
+      kyaml = <<-KYAML
+      [
+        [
+          1,
+          2,
+        ],
+      ]
+      KYAML
+      inner = [KYAML::Any.new(1), KYAML::Any.new(2)]
+      outer = [KYAML::Any.new(inner)]
+      KYAML.emit(outer).should eq(kyaml)
+    end
+  end
 end
